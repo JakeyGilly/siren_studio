18e44d53d952996b0d72fcaedcefd2a1e56c00d7
Jake Gillman <jakegillman3@gmail.com>
2024-10-26 17:12:39 +0100

    music works

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 "use client";
+import { useRef } from "react";
 
 export default function Home() {
-	return <div></div>;
+	const audioRef = useRef<HTMLAudioElement>(null);
+
+	function play() {
+		if (audioRef.current) {
+			audioRef.current.play();
+		}
+	}
+
+	return (
+		<div className="col">
+			<button onClick={play}>Click</button>
+			<audio
+				ref={audioRef}
+				src="ukelele.mp3"></audio>
+		</div>
+	);
 }
